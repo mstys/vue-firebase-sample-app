159f56dd7eea63307acbf3d6c683469a57bf27b3
Michał Styś <michal.stys@vml.com>
2019-02-08 10:53:51 +0100

    Add SPA routing middleware

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const history = require('connect-history-api-fallback');
 const serveStatic = require('serve-static');
 const path = require('path');
 
 const app = express();
+app.use(history({ verbose: true }));
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 80;
